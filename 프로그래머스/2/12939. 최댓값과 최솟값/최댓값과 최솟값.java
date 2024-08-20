0c968c55ee89ccfff532e386a7256b8469f539d5
Minhyeok1995 <76654360+minhyeok2487@users.noreply.github.com>
2024-08-20 21:27:54 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 16.64 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public String solution(String s) {        
+        int[] nums = Arrays.stream(s.split(" "))
+                           .mapToInt(Integer::parseInt)
+                           .toArray();
+
+        int min = Arrays.stream(nums).min().getAsInt();
+        int max = Arrays.stream(nums).max().getAsInt();
+        
+        return min + " " + max;
+    }
+}
