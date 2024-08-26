abc7296669bd32c09aaa13dedf53570e00355289
Minhyeok1995 <repeater2487@naver.com>
2024-08-27 08:09:57 +0900

    DFS 문제 1

---
+++ src/section06_DFS/problem01/Solution.java
@@ -0,0 +1,62 @@
+package section06_DFS.problem01;
+
+import java.util.*;
+
+class Solution {
+
+    int answer, target, stop;
+    ArrayList<Integer> nums;
+    int[] ch;
+    boolean flag;
+
+    public void DFS(int L, int number) {
+        if (flag) return;
+        if (L == stop) {
+            if (number > target) {
+                answer = number;
+                flag = true;
+            }
+        } else {
+            for (int i = 0; i < stop; i++) {
+                if (ch[i] == 0) {
+                    ch[i] = 1;
+                    DFS(L + 1, number * 10 + nums.get(i));
+                    ch[i] = 0;
+                }
+            }
+        }
+    }
+
+    public int solution(int n) {
+        // 초기화
+        answer = 0;
+        flag = false;
+        nums = new ArrayList<>();
+        target = n;
+
+        // 숫자 분리
+        int tmp = n;
+        while (tmp > 0) {
+            int t = tmp % 10;
+            nums.add(t);
+            tmp = tmp / 10;
+        }
+        nums.sort(Comparator.comparingInt(a -> a));
+
+        // DFS
+        stop = nums.size();
+        ch = new int[stop];
+        DFS(0, 0);
+        if (!flag) return -1;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution T = new Solution();
+        System.out.println(T.solution(123));
+        System.out.println(T.solution(321));
+        System.out.println(T.solution(20573));
+        System.out.println(T.solution(27711));
+        System.out.println(T.solution(54312));
+    }
+}
