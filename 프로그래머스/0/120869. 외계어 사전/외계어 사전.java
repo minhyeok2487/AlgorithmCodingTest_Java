db0eeaef9b212f04649379ff2c462d3ab8d1ad8e
Minhyeok1995 <76654360+minhyeok2487@users.noreply.github.com>
2024-10-21 11:36:34 +0900

    [level 0] Title: 외계어 사전, Time: 0.18 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i = 0; i < dic.length; i++) {
             int answer = 0;
             for(int j = 0; j < spell.length; j++) {
-                if(dic[i].contains(spell[j])) answer ++;
+                if(dic[i].contains(spell[j])) answer++;
             }
             if(answer == spell.length) return 1;
         }
