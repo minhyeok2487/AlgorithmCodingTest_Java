8da62748af67847f9d158e33a36083e44b5bb35b
Minhyeok1995 <76654360+minhyeok2487@users.noreply.github.com>
2024-10-26 20:16:27 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 77.72 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -2,39 +2,45 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] gems) {
-        // 고유한 보석 종류를 추출
-        Set<String> uniqueGems = new HashSet<>(Arrays.asList(gems)); 
+        int[] answer = new int[2];
+        Set<String> gemKinds = new HashSet<>(Arrays.asList(gems));
+        int kindCount = gemKinds.size();
         
-        // 고유한 보석의 수
-        int gemTypes = uniqueGems.size(); 
-        
-        // 초기화
+        // 현재 구간에 포함된 보석들의 개수를 저장하는 Map
         Map<String, Integer> gemCount = new HashMap<>();
-        int start = 0, end = 0;
+        
+        int start = 0;
+        int end = 0;
         int minLength = Integer.MAX_VALUE;
-        int[] answer = {0, gems.length - 1};
-
-        while (end < gems.length) {
-            // 현재 보석을 맵에 추가하고 end 포인터를 증가
-            gemCount.put(gems[end], gemCount.getOrDefault(gems[end], 0) + 1);
-            end++;
-            
-            // 윈도우가 모든 보석 종류를 포함하면 윈도우 축소 시도
-            while (gemCount.size() == gemTypes) {
-                if (end - start < minLength) {
+        int minStart = 0;
+        
+        while(true) {
+            // 모든 종류의 보석을 포함할 때까지 end 포인터 이동
+            if(gemCount.size() < kindCount && end < gems.length) {
+                gemCount.put(gems[end], gemCount.getOrDefault(gems[end], 0) + 1);
+                end++;
+            }
+            // 모든 종류의 보석을 포함하면 start 포인터 이동
+            else if(gemCount.size() == kindCount) {
+                if(end - start < minLength) {
                     minLength = end - start;
-                    answer[0] = start + 1;  // 1-based 인덱스로 변환
-                    answer[1] = end;  // end는 이미 1 증가된 상태이므로 그대로 사용
+                    minStart = start;
                 }
                 
-                // start 포인터를 증가시켜 윈도우 축소
                 gemCount.put(gems[start], gemCount.get(gems[start]) - 1);
-                if (gemCount.get(gems[start]) == 0) {
+                if(gemCount.get(gems[start]) == 0) {
                     gemCount.remove(gems[start]);
                 }
                 start++;
             }
+            // 더 이상 진행할 수 없는 경우
+            else {
+                break;
+            }
         }
+        
+        answer[0] = minStart + 1;
+        answer[1] = minStart + minLength;
         
         return answer;
     }
