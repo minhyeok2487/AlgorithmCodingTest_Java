ca9e1adb4c3b7084e550b02d7cd62519dc300a12
Minhyeok1995 <76654360+minhyeok2487@users.noreply.github.com>
2024-12-06 13:21:13 +0900

    [level 2] Title: 더 맵게, Time: 1366.24 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,26 +1,27 @@
-import java.util.PriorityQueue;
+import java.util.*;
 
 class Solution {
     public int solution(int[] scoville, int K) {
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        
-        // 배열의 모든 요소를 우선순위 큐에 추가
-        for (int s : scoville) {
-            minHeap.add(s);
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i : scoville) {
+            pq.add(i);
         }
         
-        int mixCount = 0;
-
-        // 가장 작은 스코빌 지수가 K보다 작을 때만 반복
-        while (minHeap.size() > 1 && minHeap.peek() < K) {
-            int firstMin = minHeap.poll();  // 가장 작은 값
-            int secondMin = minHeap.poll(); // 두 번째로 작은 값
-            int newScoville = firstMin + (secondMin * 2); // 새로운 스코빌 지수 계산
-            minHeap.add(newScoville);
-            mixCount++;
+        while(!pq.isEmpty()) {
+            int start = pq.poll();
+            if (start >= K) {
+                return answer;
+            } else {
+                if (pq.isEmpty()) {
+                    return -1;
+                } else {
+                    int next = pq.poll();
+                    pq.add(start + (next) * 2);
+                    answer++;
+                }
+            }
         }
-        
-        // 반복이 끝난 후에도 가장 작은 스코빌 지수가 K보다 작으면 -1 반환
-        return minHeap.peek() < K ? -1 : mixCount;
+        return -1;
     }
 }
