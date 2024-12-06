f5ab8c55455e312c3497f97af32dcdc3e811341f
Minhyeok1995 <76654360+minhyeok2487@users.noreply.github.com>
2024-12-06 17:12:39 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 39.35 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,26 +1,22 @@
 import java.util.*;
 
 public class Solution {
-    public int[] solution(int[] arr) {
+    public int[] solution(int []arr) {
+        Deque<Integer> queue = new ArrayDeque<>();
+        queue.addLast(arr[0]);
         
-        Deque<Integer> deque = new ArrayDeque<>();
         for (int i : arr) {
-            if (deque.peekLast() == null) {
-                deque.offer(i);
-            } else {
-                if (!deque.peekLast().equals(i)) {
-                    deque.offer(i);
-                }
-            }            
+            Integer check = queue.peekLast();
+            if (check != i) {
+                queue.addLast(i);
+            }
         }
-        
-        // deque의 요소를 answer 배열로 변환
-        int[] answer = new int[deque.size()];
-        int index = 0;
-        for (int num : deque) {
-            answer[index++] = num;
+        int[] answer = new int[queue.size()];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = queue.pollFirst();
         }
-        
+
         return answer;
     }
 }
