feefea1da3487f07927bbebb74048a581a4e9731
Minhyeok1995 <76654360+minhyeok2487@users.noreply.github.com>
2024-12-06 12:35:17 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 4.41 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,34 +2,37 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] jobs) {
-        // 작업을 요청 시점 순으로 정렬
-        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+        int answer = 0;
+        int end = 0; // 수행되고난 직후의 시간
+		int jobsIdx = 0; // jobs 배열의 인덱스
+		int count = 0; // 수행된 요청 갯수
         
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-        int currentTime = 0;
-        int totalResponseTime = 0;
-        int jobIndex = 0;
-        int jobCount = jobs.length;
-
-        while (jobIndex < jobCount || !pq.isEmpty()) {
-            // 현재 시점에 처리할 수 있는 작업들을 우선순위 큐에 넣음
-            while (jobIndex < jobCount && jobs[jobIndex][0] <= currentTime) {
-                pq.add(jobs[jobIndex]);
-                jobIndex++;
-            }
+        // 원본 배열 오름차순 정렬
+        Arrays.sort(jobs, Comparator.comparingInt(a -> a[0]));
+        
+        // 처리 시간 오름차순으로 정렬되는 우선순위 큐(Heap)
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+        
+        // 요청이 모두 수행될 때까지 반복
+		while (count < jobs.length) {
+         
+            // 하나의 작업이 완료되는 시점(end)까지 들어온 모든 요청을 큐에 넣음
+			while (jobsIdx < jobs.length && jobs[jobsIdx][0] <= end) {
+				pq.add(jobs[jobsIdx++]);
+			}
             
-            // 처리 가능한 작업이 있는 경우
-            if (!pq.isEmpty()) {
-                int[] currentJob = pq.poll();
-                currentTime += currentJob[1];
-                totalResponseTime += currentTime - currentJob[0];
-            } else {
-                // 현재 시점에서 처리할 작업이 없다면 시간을 다음 작업의 요청 시점으로 이동
-                currentTime = jobs[jobIndex][0];
-            }
+            // 큐가 비어있다면 작업 완료(end) 이후에 다시 요청이 들어온다는 의미
+			// (end를 요청의 가장 처음으로 맞춰줌)
+			if (pq.isEmpty()) {
+				end = jobs[jobsIdx][0];
+			// 작업이 끝나기 전(end 이전) 들어온 요청 중 가장 수행시간이 짧은 요청부터 수행
+			} else {
+				int[] temp = pq.poll();
+				answer += temp[1] + end - temp[0];
+				end += temp[1];
+				count++;
+			}
         }
-        
-        // 평균 응답 시간 계산
-        return totalResponseTime / jobCount;
+        return (int) Math.floor(answer / jobs.length);
     }
 }
